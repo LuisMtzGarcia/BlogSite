79e88528093a3992abf476dc4216b7722450e8c1
LuisMtzGarcia <ing.luismtzgarcia@gmail.com>
2022-06-20 23:19:30 -0500

    Create BlogSite application via Wagtail

---
+++ BlogSite/BlogSite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-)9rs)yo_0&3fx6x*r)r70q3r6$(9o#f5f4*zz$8w^*$m2i_!na"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
